e188d498d41f49f8e7ca35c2a62f51b87aedf666
NoeDimitri <justind4@usf.edu>
2023-03-11 11:53:29 -0500

    moving stuff over to desktop
    reworked the grid, working on checking for a winner

--- JustinMinigame/board.gd
+++ JustinMinigame/board.gd
@@ -1,21 +1,67 @@
 extends Node2D
 
 var boardPieces = []
+var boardHeight = []
 var currentPlayer
+var pieceDict = {
+	1 : 1,
+	-1: 2
+}
+
+@export var maxHeight: int = 6
+@export var maxWidth: int = 7
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	currentPlayer = 1
-	for i in range(7):
-		boardPieces.append([])
+	for i in range(maxWidth):
+		boardPieces.append([0,0,0,0,0,0])
+		boardHeight.append(0)
 	
 	
 func addPiece(columnNum):
-	boardPieces[columnNum-1].append(currentPlayer)
+	if boardHeight[columnNum-1] >= maxHeight:
+		#print("too big!!")
+		return
+	boardPieces[columnNum-1][boardHeight[columnNum-1]] = pieceDict[currentPlayer]
+	boardHeight[columnNum-1] += 1
 	currentPlayer *= -1
+	checkWin(columnNum)
+	print(checkWin(columnNum))
 	showBoard()
 
 func showBoard():
 	for row in boardPieces:
 		print(row)
 	print("---------")
+
+func checkWin(columnChosen):
+	var xCoord = columnChosen
+	var yCoord = boardHeight[columnChosen-1]
+	var playerNum = pieceDict[currentPlayer]
+	#Horizontal Check
+	#print(extendDirection(xCoord, yCoord, 1, 0, playerNum) + extendDirection(xCoord, yCoord, -1, 0, playerNum))
+	if extendDirection(xCoord, yCoord, 1, 0, playerNum) + extendDirection(xCoord, yCoord, -1, 0, playerNum) >= 5:
+		return true
+	#Vertical Check
+	if extendDirection(xCoord, yCoord, 0, 1, playerNum) + extendDirection(xCoord, yCoord, 0, -1, playerNum) >= 5:
+		return true
+	#top right Diagonal
+	if extendDirection(xCoord, yCoord, 1, 1, playerNum) + extendDirection(xCoord, yCoord, -1, -1, playerNum) >= 5:
+		return true
+	#top left diagonal
+	if extendDirection(xCoord, yCoord, -1, 1, playerNum) + extendDirection(xCoord, yCoord, 1, -1, playerNum) >= 5:
+		return true
+	return false
+	
+func extendDirection(xPos, yPos, xDelta, yDelta, playerNum):
+	#print(xPos, " ", yPos)
+	if not checkValidSpace(xPos, yPos) or boardPieces[xPos][yPos] != playerNum:
+		return 0
+	return 1 + extendDirection(xPos+xDelta, yPos + yDelta, xDelta, yDelta, playerNum)
+
+func checkValidSpace(xCoord, yCoord):
+	if(xCoord < 0 or xCoord >= maxWidth or yCoord < 0 or yCoord >= maxHeight):
+		return false
+	return true
+
